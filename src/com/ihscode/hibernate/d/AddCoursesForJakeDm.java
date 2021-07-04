31c9b9bf75414b0d04cd80daa051889f8e1accc7
Admin i <ihsanmpm@gmail.com>
2021-07-04 10:47:37 +0530

    .

---
+++ src/com/ihscode/hibernate/d/AddCoursesForJakeDm.java
@@ -0,0 +1,63 @@
+package com.ihscode.hibernate.d;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ihscode.hibernate.d.entity.Course;
+import com.ihscode.hibernate.d.entity.Instructor;
+import com.ihscode.hibernate.d.entity.InstructorDetail;
+import com.ihscode.hibernate.d.entity.Review;
+import com.ihscode.hibernate.d.entity.Student;
+
+public class AddCoursesForJakeDm {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session  = factory.getCurrentSession();
+		
+		try {
+
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// get the students Jake from database
+			int studentId = 2;
+			Student tempStudent = session.get(Student.class, studentId);
+			
+			// create more courses
+			
+			// add student to courses
+			
+			// save the courses
+
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!!!");
+			
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+					
+
+	}
+
+}
